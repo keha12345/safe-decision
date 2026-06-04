ab44c3526e146594591fdac86fb6637e1529f20d
Kirill-Kukuliev <keha12345@gmail.com>
2026-06-04 18:43:44 +0300

    add amocrm form

--- react-vite/src/hooks/useForm.tsx
+++ react-vite/src/hooks/useForm.tsx
@@ -10,16 +10,41 @@ export default function useForm(def:object){
         setData((prev: any)=>({...prev, [e.target.name]: e.target.value}))
     }
     return [handler, function(){
-        fetch(`https://api.telegram.org/bot${'7231741401:AAHRDQb18fZU7MN2mlwq7CRmlM6Ru2ft6Sg'}/sendMessage`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              chat_id: 538898722,//538898722,//323811652,
-              text: `Site client's request: \n\n ${JSON.stringify(data)}`,
-            })
-        });
+        // fetch(`https://api.telegram.org/bot${'7231741401:AAHRDQb18fZU7MN2mlwq7CRmlM6Ru2ft6Sg'}/sendMessage`, {
+        //     method: 'POST',
+        //     headers: {
+        //       'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //       chat_id: 538898722,//538898722,//323811652,
+        //       text: `Site client's request: \n\n ${JSON.stringify(data)}`,
+        //     })
+        // });
+         try {
+            // Ищем форму внутри контейнера amoCRM (замените селектор, если у вас другой ID контейнера)
+            const amoForm = document.querySelector('#amoforms_form') as HTMLFormElement;
+            // const amoSubmitButton = amoForm.querySelector('#button_submit') as HTMLButtonElement;
+
+            if (amoForm) {
+                const amoField = amoForm.querySelector('textarea[type*="text"]') as HTMLInputElement;
+                amoField.value = JSON.stringify(data, null, 4);
+                amoField.dispatchEvent(new Event('input', { bubbles: true }));
+
+                const formData = new FormData(amoForm);
+                fetch(amoForm.action, {
+                    method: 'POST',
+                    body: formData, // Браузер сам выставит нужный multipart/form-data
+                    mode: 'no-cors' // Отключаем жесткую проверку CORS, так как шлюз amoCRM внешний
+                })
+                .then(() => console.log('Данные успешно улетели в amoCRM'))
+                .catch(err => console.error('Ошибка отправки fetch:', err));
+                // setTimeout(() => amoSubmitButton.click(),1);
+            } else {
+                console.warn("Скрытая форма amoCRM еще не успела загрузиться на страницу.");
+            }
+        } catch (error) {
+            console.error("Ошибка зеркалирования в amoCRM:", error);
+        }
         navigate(prefix+'/grat');
     }, data]
 }
